2c5128528956f9794ca1f139d85bd3b899cacd79
Tawhidul Arefin <tawhidularefin530@gmail.com>
2022-08-31 14:32:36 +0600

    Added: The Sample App

---
+++ UiOnboardingApp/UiOnboardingApp/UiOnboardingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UiOnboardingAppApp.swift
+//  UiOnboardingApp
+//
+//  Created by Md Najmuzzaman on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct UiOnboardingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
